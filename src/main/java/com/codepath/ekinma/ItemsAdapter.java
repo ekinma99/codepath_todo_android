adf5fa7626d908b5650282f11c6273816c2df319
ekinma99 <ekinmakon1@student.gsu.edu>
2021-01-24 07:56:43 -0500

    Add files

--- src/main/java/com/codepath/ekinma/ItemsAdapter.java
+++ src/main/java/com/codepath/ekinma/ItemsAdapter.java
@@ -12,24 +12,23 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> 
     public ItemsAdapter(List<String> items) {
         this.items = items;
     }
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new ViewHolder(todoView);
+    }
+    public int getItemCount() {
+        return items.size();
+    }
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String item = items.get(position);
+        holder.bind(item);
+    }
     class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvItem;
         public ViewHolder(View itemView) {
             super(itemView);
             tvItem = itemView.findViewById(android.R.id.text1);
         }
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View todoView = LayoutInflater.from(parent.getContent()).inflate(android.R.layout.simple_list_item_1, parent, false);
-        return new Viewholder(todoView);
-    }
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        String item = items.get(position);
-        holder.bind(item);
-    }
-
-    public int getItemCount() {
-        return items.size();
-    }
         public void bind(String item) {
             tvItem.setText(item);
         }
--- src/main/java/com/codepath/ekinma/MainActivity.java
+++ src/main/java/com/codepath/ekinma/MainActivity.java
@@ -5,7 +5,7 @@ package com.codepath.ekinma;
         import android.os.Bundle;
         import java.util.ArrayList;
         import java.util.List;
-
+        import android.view.View;
         import androidx.recyclerview.widget.LinearLayoutManager;
         import androidx.recyclerview.widget.RecyclerView;
         import android.widget.Button;
@@ -14,22 +14,30 @@ public class MainActivity extends AppCompatActivity {
     Button btnAdd;
     TextView etItem;
     RecyclerView rvItems;
-    List <String> items;
+    ItemsAdapter itemsAdapter;
+    List<String> items;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        items= new ArrayList<String>();
+        setContentView(R.layout.activity_main);
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+        //etItem.setText("Lord, what's this mess!");
+        items = new ArrayList<String>();
         items.add("Java");
         items.add("Kotlin");
         items.add("Javascript");
-        setContentView(R.layout.activity_main);
-        btnAdd= findViewById(R.id.btnAdd);
-        etItem=findViewById(R.id.etItem);
-        rvItems=findViewById(R.id.rvItems);
-       // etItem.setText("Lord, what's this mess!");
-        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        final ItemsAdapter itemsAdapter = new ItemsAdapter(items);
         rvItems.setAdapter(itemsAdapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
-
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String todoItem = etItem.getText().toString();
+                itemsAdapter.notifyItemInserted(items.size() - 1);
+                etItem.setText("");
+            }
+        });
     }
 }
